cb86ac1c13e2d0968713418de85e5d1036a3b495
BenGH28 <45215137+BenGH28@users.noreply.github.com>
2020-04-29 19:44:43 -0600

    Quickly install all the packages

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+#installing my favourite packages and utilities
+sudo pacman -S alacritty i3 i3-gaps picom rofi ttf-font-awesome i3exit
+
+#install those utilities that aren't on ARCH repos
+#Oh-my-zsh
+ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#vim-plugged for vim
+ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
