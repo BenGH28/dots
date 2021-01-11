37c7d0f34a02c633830e2d6e8009fc8c5f381d19
Ben <bigben.hunt@gmail.com>
2021-01-10 19:55:47 -0700

    remove lshift command

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -4,6 +4,5 @@ nitrogen --restore &
 nm-applet &
 xfce4-power-manager &
 pamac-tray &
-lshift &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 betterlockscreen -u ~/Pictures/Wallpapers/sea_rocks_moss_132634_1920x1080.jpg -l blur --off 300 &
