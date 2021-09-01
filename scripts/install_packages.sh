e00ed0115f0ea5f9380da56235f155dcad3aa2b2
Ben Hunt <bigben.hunt@gmail.com>
2021-09-01 15:33:56 -0600

    Setup: Update base packages for initial install
    Replace betterlockscreen with slock b/c it takes too long to start up
    when enabled in systemd

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-   #installing my favourite packages and utilities
-install_pkgs(){
+#installing my favourite packages and utilities
+install_pkgs() {
     echo "installing pacman packages"
-	sudo pacman -Syu --noconfirm --needed - < $HOME/scripts/pacman-pkgs.txt
+    sudo pacman -Syu --noconfirm --needed - <$HOME/scripts/pacman-pkgs.txt
 }
 
 ##################################################
@@ -11,31 +11,31 @@ install_pkgs(){
 ##################################################
 
 #get the aur-packages
-install_aur_pkg(){
+install_aur_pkg() {
     echo "installing aur packages"
-    yay -S < $HOME/scripts/aur-pkgs.txt
+    yay -S <$HOME/scripts/aur-pkgs.txt
 }
 
 #get my version of dmenu
-install_dmenu(){
-	echo "Cloning dmenu-plusplus from github"
-	git clone https://github.com/BenGH28/dmenu-plusplus.git ~/.config/dmenu/
-	cd ~/.config/dmenu
-	sudo make clean install
-	cd
+install_dmenu() {
+    echo "Cloning dmenu-plusplus from github"
+    git clone https://github.com/BenGH28/dmenu-plusplus.git ~/.config/dmenu/
+    cd ~/.config/dmenu
+    sudo make clean install
+    cd
 }
 
 # install rustup to gain access to cargo
-install_rust(){
-	echo "Installing rust" && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+install_rust() {
+    echo "Installing rust" && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 }
 
 # installing commandline to for changing your alacritty theme
-install_alacritty_theme(){
-	echo "Installing alacritty-theme from cargo" &&  ~/.cargo/bin/cargo install alacritty-theme
+install_alacritty_theme() {
+    echo "Installing alacritty-theme from cargo" && ~/.cargo/bin/cargo install alacritty-theme
 }
 
-install_starship_prompt(){
+install_starship_prompt() {
     echo "installing starship..." && curl -fsSL https://starship.rs/install.sh | bash
 }
 
@@ -49,34 +49,35 @@ install_starship_prompt
 # link the appropriate scripts in the appropriate places
 echo "linking your scripts to your PATH (~/.local/bin)"
 dir=$HOME/.local/bin
-check_if_in_path(){
-	result=$(echo $PATH | grep $dir)
-	case $result in
-		"") echo "$dir is not in your \$PATH please add it to \$PATH"
-			exit
-			;;
-		$PATH) echo "$dir is in \$PATH"
-			;;
-		*) echo "uh-oh";;
-	esac
+check_if_in_path() {
+    result=$(echo $PATH | grep $dir)
+    case $result in
+        "")
+            echo "$dir is not in your \$PATH please add it to \$PATH"
+            exit
+            ;;
+        $PATH)
+            echo "$dir is in \$PATH"
+            ;;
+        *) echo "uh-oh" ;;
+    esac
 }
 
-linking_to_path(){
-	echo "placing themile in your PATH"
-	if [ -d $dir ]; then
-		ln -s ~/scripts/themile ~/.local/bin/
-	else
-		echo "$dir does not exist! Making it now!"
-		mkdir -p $HOME/.local/bin/
-		ln -s ~/scripts/themile ~/.local/bin/
-	fi
-	echo "linking complete!"
+linking_to_path() {
+    echo "placing themile in your PATH"
+    if [ -d $dir ]; then
+        ln -s ~/scripts/themile ~/.local/bin/
+    else
+        echo "$dir does not exist! Making it now!"
+        mkdir -p $HOME/.local/bin/
+        ln -s ~/scripts/themile ~/.local/bin/
+    fi
+    echo "linking complete!"
 }
 
 check_if_in_path
 
 linking_to_path
 
-echo "backing up your .profile and linking $HOME/.config/zsh/profile to .zprofile" && \
+echo "backing up your .zprofile and linking $HOME/.config/zsh/profile to .zprofile" &&
     ln -bfs $HOME/.config/zsh/profile $HOME/.zprofile
-
