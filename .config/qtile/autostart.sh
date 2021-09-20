eea1f9432570af5d9137da5d003ad377520a3b76
Ben Hunt <bigben.hunt@gmail.com>
2021-09-20 00:15:02 -0600

    qtile: update auto start to support multi-monitor setup

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -7,6 +7,11 @@ xfce4-power-manager &
 pamac-tray &
 accountable2you &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-betterlockscreen -u ~/Pictures/arcolinux/soft-morning-3200x2000.jpg
 dunst &
 xmodmap ~/.Xmodmap &
+
+#if not a laptop set my displays accordingly
+if [ ! -f /sys/class/power_supply/BAT0/model_name]; then
+    xrandr --output DP-2 --mode 1920x1080 --rate 60 --primary
+    xrandr --output HDMI-0 --mode 1920x1080 --rate 60 --noprimary --right-of DP-2
+fi
