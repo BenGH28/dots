704d63558ae42d959be5176b1bcf82bc165b85d4
Ben Hunt <bigben.hunt@gmail.com>
2021-09-24 20:46:05 -0500

    remove gui stuff

--- .config/polybar/launch.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-
-#terminate all instances of polybar
-killall -q polybar
-
-polybar -c $HOME/.config/polybar/config bar1 &
--- .config/qtile/xephyr-env.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-#to test the qtile config run this and then
-Xephyr -br -ac -noreset -screen 1280x720 :1 &
-
-DISPLAY=:1 exec qtile start
