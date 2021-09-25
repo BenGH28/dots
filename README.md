63f3af4c422a9ebbc565965a6ece95a997594f89
Ben Hunt <bigben.hunt@gmail.com>
2021-09-25 00:41:16 -0500

    remove superfluous stuff for WSL

--- README.md
+++ README.md
@@ -13,8 +13,3 @@ install packages related to the dotfiles
 ```sh
 scripts/install_packages.sh
 ```
-
-## Preview
-
-![Qtile Layout](https://github.com/BenGH28/dots/blob/master/.screenshots/qconf_conf1.png)
-![Qtile OneDark](https://github.com/BenGH28/dots/blob/master/.screenshots/qtile_OneDarkTheme.png)
--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -16,31 +16,13 @@ install_aur_pkg() {
     yay -S - <~/scripts/aur-pkgs.txt
 }
 
-#get my version of dmenu
-install_dmenu() {
-    echo "Cloning dmenu-plusplus from github"
-    git clone https://github.com/BenGH28/dmenu-plusplus.git ~/.config/dmenu/
-    cd ~/.config/dmenu
-    sudo make clean install
-    cd
-}
-
 # install rustup to gain access to cargo
 install_rust() {
     echo "Installing rust" && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 }
 
-# installing commandline to for changing your alacritty theme
-install_alacritty_theme() {
-    echo "Installing alacritty-theme from cargo" && ~/.cargo/bin/cargo install alacritty-theme
-}
-
 install_starship_prompt() {
     echo "installing starship..." && curl -fsSL https://starship.rs/install.sh | bash
-}
-
-install_psutil() {
-    echo "installing psutil for Qtile..." && pip install psutil
 }
 
 install_tpm() {
@@ -51,23 +33,14 @@ install_nvim() {
     echo "installing my nvim config..." && curl https://raw.githubusercontent.com/BenGH28/nvim/main/bootstrap.sh | sh
 }
 
-setup_xbacklight() {
-    echo "adding '$USER' to the video group..." && sudo usermod -a -G video $USER
-    echo "setup acpibacklight..." && sudo cp backlight.rules /etc/udev/rules.d/90-backlight.rules
-}
-
 change_shell() {
     echo "changing shell to zsh..." && chsh -s /bin/zsh
 }
 
 install_pkgs
 install_aur_pkg
-install_dmenu
 install_rust
-install_alacritty_theme
 install_starship_prompt
 install_tpm
-install_psutil
 install_nvim
-setup_xbacklight
 change_shell
