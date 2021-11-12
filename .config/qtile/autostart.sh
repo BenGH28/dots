bbbe9674fac2fc0083eb160b8e9fe958698f092f
Ben Hunt <bigben.hunt@gmail.com>
2021-11-11 18:14:25 -0700

    move xrandr to .xprofile

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -9,8 +9,3 @@ accountable2you &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 dunst &
 xmodmap ~/.Xmodmap &
-
-#if not a laptop set my displays accordingly
-[ ! -f /sys/class/power_supply/BAT0/model_name ] &&
-    xrandr --output DP-2 --mode 1920x1080 --rate 60 --primary &&
-    xrandr --output HDMI-0 --mode 1920x1080 --rate 60 --noprimary --right-of DP-2
