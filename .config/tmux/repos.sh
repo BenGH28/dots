7fdaee027183c48122b3c4696b74ddb2988625b9
Ben Hunt <bigben.hunt@gmail.com>
2021-11-17 15:35:29 -0700

    tmux: add some comments for future me

--- .config/tmux/repos.sh
+++ .config/tmux/repos.sh
@@ -7,9 +7,14 @@ choice=$(/usr/bin/ls -A $HOME/repos | fzf --reverse --prompt="project: ")
 exists=$(tmux list-sessions | grep "$choice" | awk '{print $1}' | sed 's/://')
 
 case $exists in
-    $choice) tmux switch-client -t $choice ;;
+    $choice)
+        #it already exists so just switch to it
+        tmux switch-client -t $choice
+        ;;
     *)
+        # make a new session
         tmux new-session -d -c $HOME/repos/$choice -s $choice
+        # switch to that session
         tmux switch-client -t $choice
         ;;
 esac
