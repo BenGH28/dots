b5ddd5338e3fe5d7959e453be9f36464c0484883
Ben Hunt <45215137+BenGH28@users.noreply.github.com>
2022-02-24 00:48:31 -0700

    add general updates

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env sh
 
-picom --experimental-backends &
-nitrogen --restore &
-nm-applet &
-blueman-applet &
-xfce4-power-manager &
-pamac-tray &
-accountable2you &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-dunst &
-xmodmap ~/.Xmodmap &
+function run {
+	if ! pgrep -f $1; then
+		$@ &
+	fi
+}
+
+run picom --experimental-backends
+run nitrogen --restore
+run nm-applet
+run blueman-applet
+run xfce4-power-manager
+run pamac-tray
+run accountable2you
+run /usr/lib/polkit-kde-authentication-agent-1
+run dunst
+run xmodmap ~/.Xmodmap
