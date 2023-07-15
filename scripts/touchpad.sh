112254658dc6b155ca9c3094d563a06669218ad1
Ben Hunt <bnjmnhnt.git@gmail.com>
2023-07-14 19:59:22 -0600

    add script to enable my touch pad on dell inspiron 5515

---
+++ scripts/touchpad.sh
@@ -0,0 +1,23 @@
+#/usr/bin/env bash
+MY_MOD=i2c_hid_acpi
+
+remove_mod(){
+    sudo rmmod $MY_MOD
+}
+
+add_mod(){
+    sudo modprobe $MY_MOD
+}
+
+main(){
+    local module_present=$(lsmod | grep $MY_MOD)
+    # check if the module is present
+    if [[ -z $module_present ]]; then
+        # we can't remove something that isn't there so just add the module
+        add_mod
+    else
+        remove_mod && add_mod
+    fi
+}
+
+main
