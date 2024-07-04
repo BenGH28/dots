ba743d90a9e522a03cffb06a2b35017f46115f14
Ben Hunt <bnjmnhnt.git@gmail.com>
2024-07-03 18:24:41 -0600

    hyprland: sort out hyprlock config

--- .config/hypr/autostart.sh
+++ .config/hypr/autostart.sh
@@ -12,3 +12,4 @@ run hypridle
 run blueman-applet
 run accountable2you
 run /usr/lib/polkit-kde-authentication-agent-1
+run wl-paste -p -t text --watch clipman store -P --histpath="~/.local/share/clipman-primary.json"
