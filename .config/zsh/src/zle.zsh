3501b4b38fcd7ad6700bbc33e55d5ad213dd7055
Ben Hunt <bnjmnhnt.git@gmail.com>
2024-07-05 18:07:27 -0600

    add zle keybinding for opening files in neovim using fzf

--- .config/zsh/src/zle.zsh
+++ .config/zsh/src/zle.zsh
@@ -4,68 +4,77 @@
 
 # Change cursor shape for different vi modes.
 zle-keymap-select() {
-	case $KEYMAP in
-		vicmd) echo -ne '\e[1 q' ;;        # block
-		viins | main) echo -ne '\e[5 q' ;; # beam
-	esac
+    case $KEYMAP in
+        vicmd) echo -ne '\e[1 q' ;;        # block
+        viins | main) echo -ne '\e[5 q' ;; # beam
+    esac
 }
 zle -N zle-keymap-select
 
 zle-line-init() {
-	zle -K viins # initiate `vi insert` as keymap
-	echo -ne '\e[5 q'
+    zle -K viins # initiate `vi insert` as keymap
+    echo -ne '\e[5 q'
 }
 zle -N zle-line-init
 ####
 
 git_or_yadm() {
-	[ -d .git ] && git $@ || yadm $@
+    [ -d .git ] && git $@ || yadm $@
 }
 # \e = Alt
 status() {
-	zle kill-whole-line
-	git_or_yadm status
-	zle accept-line
+    zle kill-whole-line
+    git_or_yadm status
+    zle accept-line
 }
 zle -N status
-# Alt+g
-bindkey '\eg' status
+bindkey '\eg' status # Alt+g
 
 addu() {
-	zle kill-whole-line
-	# add the normal stuff
-	git_or_yadm add -u
-	zle accept-line
-	# let me see that I added everything
-	git_or_yadm status
+    zle kill-whole-line
+    # add the normal stuff
+    git_or_yadm add -u
+    zle accept-line
+    # let me see that I added everything
+    git_or_yadm status
 }
 zle -N addu
-# Alt+a
-bindkey '\ea' addu
+bindkey '\ea' addu # Alt+a
 
 commit() {
-	zle kill-whole-line
-	git_or_yadm commit
-	zle accept-line
+    zle kill-whole-line
+    git_or_yadm commit
+    zle accept-line
 }
 zle -N commit
-# Alt+m
-bindkey '\em' commit
+bindkey '\em' commit # Alt+m
 
 pull() {
-	zle kill-whole-line
-	git_or_yadm pull
-	zle accept-line
+    zle kill-whole-line
+    git_or_yadm pull
+    zle accept-line
 }
 zle -N pull
-# Alt+p l
-bindkey '\epl' pull
+bindkey '\epl' pull # Alt+p l
 
 push() {
-	zle kill-whole-line
-	git_or_yadm push
-	zle accept-line
+    zle kill-whole-line
+    git_or_yadm push
+    zle accept-line
 }
 zle -N push
-# Alt+p s
-bindkey '\eps' push
+bindkey '\eps' push # Alt+p s
+
+
+fzf_open_in_nvim() {
+    local selected_file
+    selected_file=$(fd -H --type f . ~ | fzf -m --preview='bat --color=always {}')
+    if [[ -n "$selected_file" ]]; then
+        # take a multiline string turn it into an array
+        selected_file=("${(@f)selected_file}")
+        vim $selected_file
+    fi
+    zle accept-line
+}
+zle -N fzf_open_in_nvim
+bindkey '^O' fzf_open_in_nvim
