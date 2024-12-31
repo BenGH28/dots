2c5c6345b1253f0d139f2fec3e6699ee89b0bc49
Ben Hunt <bnjmnhnt.git@gmail.com>
2024-12-31 00:01:33 -0600

    install packages better

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -21,22 +21,19 @@ install_paru() {
 }
 
 install_system_pkgs() {
+    if ! command -v paru 2>/dev/null; then
+        install_rust
+        install_paru
+    fi
     message "installing packages"
-    sudo paru -Syu --noconfirm --needed - <"$HOME/scripts/packages.txt"
+
+    filename="$HOME/scripts/packages.txt"
+    sudo pacman -S --noconfirm --needed $(comm -12 <(pacman -Slq | sort) <(sort $filename))
 }
 
 # install rustup to gain access to cargo
 install_rust() {
     message "Installing rust" && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-}
-
-install_psutil() {
-    message "installing psutil for Qtile..." && pip install psutil
-}
-
-install_tpm() {
-    message "installing Tmux Plugin Manager..." &&
-        git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 }
 
 install_nvim() {
@@ -48,38 +45,37 @@ setup_xbacklight() {
     message "adding '$USER' to the video group..." &&
         sudo usermod -a -G video "$USER" &&
         message "setup acpibacklight..." &&
-        sudo cp backlight.rules /etc/udev/rules.d/90-backlight.rules
+        sudo cp /home/ben/scripts/backlight.rules /etc/udev/rules.d/90-backlight.rules
 }
 
 change_shell() {
     # see if zsh is installed and if so then change to zsh
-    if command -v zsh 2>/dev/null; then
-        message "zsh is installed"
-    else
-        message "zsh is not installed"
-        return
-    fi
-
-    message "changing shell to zsh..." && chsh -s /bin/zsh || return
-
+    command -v zsh 2>/dev/null && message "changing shell to zsh..." && chsh -s /bin/zsh || message "zsh is not installed"
 }
 
 install_snaps() {
     sudo systemctl enable snapd &&
         sudo systemctl start start snapd &&
-        sudo snap install spotify discord accountable2you
+        sudo snap install spotify discord accountable2you || echo "can't enable snapd"
 }
 
-main() {
-    install_rust
-    install_paru
+all() {
     install_system_pkgs
     install_snaps
     install_tpm
-    install_psutil
     install_nvim
     setup_xbacklight
     change_shell
 }
 
-main
+main() {
+    case "$1" in
+    all) all ;;
+    nvim) install_nvim ;;
+    tpm) install_tpm ;;
+    pkgs) install_system_pkgs ;;
+    *) all ;;
+    esac
+}
+
+main $@
