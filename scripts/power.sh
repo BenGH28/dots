3bf2aeb2725d78fdc4973ae4aefa8b5d51c89134
Ben Hunt <bnjmnhnt.git@gmail.com>
2025-01-09 00:00:19 -0700

    power: user can refresh qtile too

--- scripts/power.sh
+++ scripts/power.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+opts="Lock|Log out|Suspend|Reboot|Shutdown|Refresh"
+char="|"
+num=$(grep -o "$char" <<<"$opts" | wc -l)
 
-cmd=$(echo -e "Lock|Log out|Suspend|Reboot|Shutdown" | rofi -sep '|' -dmenu -i -l 5) 
-
+len=$(expr $num + 1)
+cmd=$(echo -e $opts | rofi -sep '|' -dmenu -i -l $len)
 case $cmd in
-    Shutdown) shutdown now ;;
-    "Log out") qtile cmd-obj -o cmd -f shutdown ;;
-    Reboot) reboot ;;
-    Lock) slock ;;
-    Suspend) systemctl suspend ;;
-    *) ;;
+Shutdown) shutdown now ;;
+"Log out") qtile cmd-obj -o cmd -f shutdown ;;
+Refresh) qtile cmd-obj -o cmd -f restart ;;
+Reboot) reboot ;;
+Lock) slock ;;
+Suspend) systemctl suspend ;;
+*) ;;
 esac
