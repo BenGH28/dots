c9df9b596fc21e470316b6d0f93667fb47b9e94b
Ben Hunt <bnjmnhnt.git@gmail.com>
2025-08-11 22:26:26 -0600

    add prompt

--- scripts/power.sh
+++ scripts/power.sh
@@ -3,8 +3,8 @@ opts="Lock|Log out|Suspend|Reboot|Shutdown|Refresh"
 char="|"
 num=$(grep -o "$char" <<<"$opts" | wc -l)
 
-len=$(expr $num + 1)
-cmd=$(echo -e $opts | rofi -sep '|' -dmenu -i -l $len)
+len=$(("$num" + 1))
+cmd=$(echo -e "$opts" | rofi -sep '|' -dmenu -i -l "$len" -p "power options")
 case $cmd in
 Shutdown) shutdown now ;;
 "Log out") qtile cmd-obj -o cmd -f shutdown ;;
