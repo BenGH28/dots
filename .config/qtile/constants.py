b4c9fa9445ea275b5b771b27fdbc52203e9b51f4
Ben Hunt <bnjmnhnt.git@gmail.com>
2025-11-16 00:01:00 -0700

    qtile: change font and size

--- .config/qtile/constants.py
+++ .config/qtile/constants.py
@@ -10,8 +10,8 @@ TERM = "kitty"
 BROWSER = "firefox"
 OPAQUE = 1
 BAR_SIZE = 35
-FONT = "Fira Code"
-FONT_SIZE = 18
-PALETTE = DRACULA
+FONT = "JetBrainsMono Nerd Font Medium"
+FONT_SIZE = 16
+PALETTE = CATPPUCCIN_LATTE
 
 QTILE_DIR = "/home/ben/.config/qtile/"
